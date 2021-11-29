c061caf9d988f8d0079aa05ffad989308708c14d
viistorrr <victormeza41@gmail.com>
2021-11-29 09:15:06 -0500

    footer css

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <div class="page-footer">
+            <div class="page-footer-inner"> 2022 &copy; hostels admin By 
+            <a href="mailto:victormeza41@gmail.com" target="_top" class="makerCss"> viistorrr</a>
+            </div>
+            <div class="scroll-to-top">
+                <i class="icon-arrow-up"></i>
+            </div>
+        </div>       
     )
 }
 
