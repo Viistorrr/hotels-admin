0e6e9a1e99929ffa9764276e897897c7e15aef8c
viistorrr <victormeza41@gmail.com>
2021-11-30 08:59:19 -0500

    App routes and AppContext settings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from "./components/layout/Layout";
 import Dashboard from "./components/pages/Dashboard";
+import NotFoundPage from './components/pages/NotFound';
+import AppContext from './context/AppContext';
+import useInitialState from './hooks/useInitialState';
+
 
 function App() {
-  return (
-    <>
-      <Layout>
-        <Dashboard/>
-       
-      </Layout>
-    </>
+  const initialState = useInitialState();
+  return (   
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Dashboard/>} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>   
+        </Layout>
+      </BrowserRouter>   
+    </AppContext.Provider> 
   );
 }
 
